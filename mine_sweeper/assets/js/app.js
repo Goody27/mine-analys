fb3324374ebcb2be50d4c0885acc91aa7fcea538
Goody27 <b0531060@eng.kitakyu-u.ac.jp>
2022-12-19 19:37:43 +0900

    change to

--- mine_sweeper/assets/js/app.js
+++ mine_sweeper/assets/js/app.js
@@ -1,6 +1,5 @@
 // We import the CSS which is extracted to its own file by esbuild.
 // Remove this line if you add a your own CSS build pipeline (e.g postcss).
-import "../css/app.css"
 
 // If you want to use Phoenix channels, run `mix help phx.gen.channel`
 // to get started and then uncomment the line below.
@@ -11,9 +10,9 @@ import "../css/app.css"
 // The simplest option is to put them in assets/vendor and
 // import them using relative paths:
 //
-//     import "../vendor/some-package.js"
+//     import "./vendor/some-package.js"
 //
-// Alternatively, you can `npm install some-package --prefix assets` and import
+// Alternatively, you can `npm install some-package` and import
 // them using a path starting with the package name:
 //
 //     import "some-package"
@@ -25,10 +24,11 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import Drag from './dragHook';
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
-
+let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}, hooks: {Drag}})
+  
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
 window.addEventListener("phx:page-loading-start", info => topbar.show())
@@ -42,4 +42,3 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
-
